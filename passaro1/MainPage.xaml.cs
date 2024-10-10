9007bc5a699cbb5bf7b170580eb668862b28542c
passaro1 <malvesdasi8@gmail.com>
2024-10-10 08:57:44 -0300

    10/10

--- passaro1/MainPage.xaml.cs
+++ passaro1/MainPage.xaml.cs
@@ -2,13 +2,36 @@
 
 public partial class MainPage : ContentPage
 {
+	const int gravidade = 30; //pixel
+	const int tempoEntreFrames = 100;
+	bool estaMorto = false;
 	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
+	void AplicaGravidade()
+	{
+		passaro.TranslationY += gravidade;
+	}
+	async Task Desenhar()
+	{
+		while (!estaMorto)
+		{
+			AplicaGravidade();
+			await Task.Delay(tempoEntreFrames);
+		}
+	}
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+		Desenhar();
+	}
 
-	
+
+
+
+
 }
 
