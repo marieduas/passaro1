272f78ae4cb4035e003c3a61359f8569b522ba57
passaro1 <malvesdasi82gmail.com>
2024-10-11 11:05:08 -0300

    11/10

--- passaro1/MainPage.xaml.cs
+++ passaro1/MainPage.xaml.cs
@@ -4,8 +4,11 @@ public partial class MainPage : ContentPage
 {
 	const int gravidade = 2; 
 	const int tempoEntreFrames = 25;
-	bool estaMorto = false;
+	bool estaMorto = true;
 	int count = 0;
+	double larguraJanela = 0;
+	double alturaJanela = 0;
+	int velocidade = 7;
 
 	public MainPage()
 	{
@@ -21,6 +24,7 @@ public partial class MainPage : ContentPage
 		{
 			AplicaGravidade();
 			await Task.Delay(tempoEntreFrames);
+			GerenciaCanos();
 		}
 	}
 	protected override void OnAppearing()
@@ -28,7 +32,34 @@ public partial class MainPage : ContentPage
 		base.OnAppearing();
 		Desenhar();
 	}
+    protected override void OnSizeAllocated(double width, double height)
+    {
+        base.OnSizeAllocated(width, height);
+		larguraJanela = width;
+		alturaJanela = height;
+    }
+    void GerenciaCanos()
+	{
+		canodecima.TranslationX -= velocidade;
+		canodebaixo.TranslationX -= velocidade;
+		if (canodebaixo.TranslationX <- larguraJanela)
+		{
+			canodebaixo.TranslationX = 0;
+			canodecima.TranslationX = 0;
+		}
+	}
+	void OnGameOverClicked (object s,TappedEventArgs args)
+	{
+		iniciar.IsVisible = false;
+		Inicializar();
+		Desenhar();
+	}
 
+	void Inicializar()
+	{
+		estaMorto = false;
+		iniciar.TranslationY = 0;
+	}
 
 
 
