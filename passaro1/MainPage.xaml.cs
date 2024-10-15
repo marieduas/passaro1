a359878fb267df5b6b63a4a7603a1148323f85f1
passaro1 <malvesdasi8@gmail.com>
2024-10-15 11:39:44 -0300

    15/10

--- passaro1/MainPage.xaml.cs
+++ passaro1/MainPage.xaml.cs
@@ -2,67 +2,122 @@
 
 public partial class MainPage : ContentPage
 {
-	const int gravidade = 2; 
+	const int gravidade = 3;
 	const int tempoEntreFrames = 25;
-	bool estaMorto = true;
-	int count = 0;
+	bool morto = true;
 	double larguraJanela = 0;
 	double alturaJanela = 0;
-	int velocidade = 7;
+	int velocidade = 9;
+	const int maxTempoPulando =3;
+	int tempoPulando = 0;
+	bool estaPulando = false;
+	const int forcaPulo =60;
+
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
-	void AplicaGravidade()
+
+	async Task Desenhar()
+	{
+		while (!morto)
+		{
+			if (estaPulando)
+				AplicarPulo();
+			else 
+				AplicarGravidade();
+			await Task.Delay(tempoEntreFrames);
+			GerenciaCanos();
+			if (VerificaColisao())
+			{
+				morto = true;
+				frameGameOver.IsVisible = true;
+				break;
+			}
+			await Task.Delay(tempoEntreFrames);
+		}
+	}
+	void AplicarGravidade()
 	{
 		passaro.TranslationY += gravidade;
 	}
-	async Task Desenhar()
+
+	protected override void OnSizeAllocated(double width, double height)
 	{
-		while (!estaMorto)
-		{
-			AplicaGravidade();
-			await Task.Delay(tempoEntreFrames);
-			GerenciaCanos();
-		}
-	}
-	protected override void OnAppearing()
-	{
-		base.OnAppearing();
-		Desenhar();
-	}
-    protected override void OnSizeAllocated(double width, double height)
-    {
-        base.OnSizeAllocated(width, height);
+		base.OnSizeAllocated(width, height);
 		larguraJanela = width;
 		alturaJanela = height;
-    }
-    void GerenciaCanos()
+	}
+
+	void GerenciaCanos()
 	{
 		canodecima.TranslationX -= velocidade;
 		canodebaixo.TranslationX -= velocidade;
-		if (canodebaixo.TranslationX <- larguraJanela)
+		if (canodebaixo.TranslationX < -larguraJanela)
 		{
-			canodebaixo.TranslationX = 0;
-			canodecima.TranslationX = 0;
+			canodebaixo.TranslationX = 100;
+			canodecima.TranslationX = 100;
 		}
 	}
-	void OnGameOverClicked (object s,TappedEventArgs args)
-	{
-		iniciar.IsVisible = false;
-		Inicializar();
-		Desenhar();
-	}
-
 	void Inicializar()
 	{
-		estaMorto = false;
-		iniciar.TranslationY = 0;
+		morto = false;
+		passaro.TranslationY = 0;
 	}
 
+	void OnGameOverClicked(object s, TappedEventArgs a)
+	{
+		frameGameOver.IsVisible = false;
+		Inicializar();
+		Desenhar();
 
+	}
 
+	bool VerificaColisao()
+	{
+		if (!morto)
+		{
+			if (VerificaColisaoTeto() ||
+				VerificaColisaoChao())
+			{
+				return true;
+			}
+				
+		}
+		return false;
+	}
+	 private bool VerificaColisaoTeto()
+	{
+		var minY =-alturaJanela/2;
+		if (passaro.TranslationY <=minY)
+
+	 	    return true;
+	    else
+			return false;
+	}
+
+	bool VerificaColisaoChao()
+	{
+		var mixY = alturaJanela/2; 
+		if (passaro.TranslationY >=mixY)
+	 		return true;
+	 else
+			return false;
+	}
+	void AplicarPulo()
+	{
+		passaro.TranslationY -=forcaPulo;
+		tempoPulando ++;
+		if (tempoPulando >=maxTempoPulando)
+		{
+			estaPulando = false;
+			tempoPulando = 0;
+		}
+	}
+	void OnGridClickd(object s,TappedEventArgs a)
+	{
+		estaPulando = true;
+	}
 
 }
-
