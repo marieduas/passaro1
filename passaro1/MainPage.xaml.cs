0d7c3627b37f5f5729cb2c437935cbe194ee659d
passaro1 <malvesdasi8@gmail.com>
2024-10-24 09:00:22 -0300

    24/10

--- passaro1/MainPage.xaml.cs
+++ passaro1/MainPage.xaml.cs
@@ -2,16 +2,16 @@
 
 public partial class MainPage : ContentPage
 {
-	const int gravidade = 3;
+	const int gravidade = 20;
 	const int tempoEntreFrames = 25;
 	bool morto = true;
 	double larguraJanela = 0;
 	double alturaJanela = 0;
-	int velocidade = 9;
+	int velocidade = 10;
 	const int maxTempoPulando =3;
 	int tempoPulando = 0;
 	bool estaPulando = false;
-	const int forcaPulo =60;
+	const int forcaPulo =40;
 	const int aberturaMinima = 200;
 	int score = 0;
 
@@ -22,8 +22,30 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+	}
+	protected override void OnSizeAllocated(double width, double height)
+	{
+		base.OnSizeAllocated(width, height);
+		larguraJanela = width;
+		alturaJanela = height;
+		if (height > 0);
+	}
+	void Inicializar()
+	{
+		passaro.TranslationY = 0;
+		passaro.TranslationX = 0;
+		score = 0;
+		canodebaixo.TranslationY =-larguraJanela;
+		canodecima.TranslationX =-larguraJanela;
 
-	async Task Desenhar()
+		GerenciaCanos();
+	}
+
+
+	private async Task Desenhar()
 	{
 		while (!morto)
 		{
@@ -31,129 +53,44 @@ public partial class MainPage : ContentPage
 				AplicarPulo();
 			else 
 				AplicarGravidade();
-			await Task.Delay(tempoEntreFrames);
+
 			GerenciaCanos();
+
+			
 			if (VerificaColisao())
 			{
 				morto = true;
+				labelLP.Text = "Você passou por " + score + "canos";
 				frameGameOver.IsVisible = true;
 				break;
 			}
 			await Task.Delay(tempoEntreFrames);
 		}
 	}
-	void AplicarGravidade()
-	{
-		passaro.TranslationY += gravidade;
-	}
-
-	protected override void OnSizeAllocated(double width, double height)
-	{
-		base.OnSizeAllocated(width, height);
-		larguraJanela = width;
-		alturaJanela = height;
-	}
-
 	void GerenciaCanos()
 	{
 		canodecima.TranslationX -= velocidade;
 		canodebaixo.TranslationX -= velocidade;
 		if (canodebaixo.TranslationX < -larguraJanela)
 		{
-			canodebaixo.TranslationX = 100;
-			canodecima.TranslationX = 100;
-			score++;
-			if (score%2==0)
-			    velocidade++;
-			labelLP.Text = "canos: "+score.ToString("D3");
+			canodebaixo.TranslationX = 0;
+			canodecima.TranslationX = 0;
+
 			var alturaMax =-100;
 			var alturaMin =-canodebaixo.HeightRequest;
+
 			canodecima.TranslationY = Random.Shared.Next((int)alturaMin,(int)alturaMax);
 			canodebaixo.TranslationY = canodecima.TranslationY + aberturaMinima + canodebaixo.HeightRequest;
+
+			score++;
+			labelLP.Text = "canos: "+score.ToString("D5");
+			if (score % 4 == 0)
+			    velocidade++;
 		}
 	}
-	void Inicializar()
+	void AplicarGravidade()
 	{
-		morto = false;
-		passaro.TranslationY = 0;
-		passaro.TranslationX = 0;
-		score = 0;
-		GerenciaCanos();
-		canodebaixo.TranslationY =-larguraJanela;
-		canodecima.TranslationX =-larguraJanela;
-	}
-
-	void OnGameOverClicked(object s, TappedEventArgs a)
-	{
-		frameGameOver.IsVisible = false;
-		Inicializar();
-		Desenhar();
-
-	}
-
-	bool VerificaColisao()
-	{
-		if (!morto)
-		{
-			return VerificaColisaoTeto() ||
-				VerificaColisaoChao() ||
-				VerificaColisaoCanoCima()||
-				VerificaColisaoCanoBaixo();
-				
-		}
-		return false;
-		
-	}
-
-	 private bool VerificaColisaoTeto()
-	{
-		var minY =-alturaJanela/2;
-		if (passaro.TranslationY <=minY)
-
-	 	    return true;
-	    else
-			return false;
-	}
-
-	bool VerificaColisaoChao()
-	{
-		var mixY = alturaJanela/2; 
-		if (passaro.TranslationY >=mixY)
-	 		return true;
-	 else
-			return false;
-	}
-	bool VerificaColisaoCanoCima()
-	{
-		var posHpassaro = (larguraJanela/2)-(passaro.WidthRequest/2);
-		var posVpassaro = (alturaJanela/2)-(passaro.HeightRequest/2)+passaro.TranslationY;
-		if (posHpassaro>= Math.Abs(canodecima.TranslationX)-canodecima.WidthRequest &&
-		    posHpassaro<= Math.Abs(canodecima.TranslationX)+canodecima.WidthRequest &&
-			posVpassaro<= canodecima.HeightRequest +canodecima.TranslationY)
-			{
-				return true;
-			}
-			else
-			{
-				return false;
-			}
-
-	}
-	bool VerificaColisaoCanoBaixo()
-	{
-		var posHpassaro = (larguraJanela/2)-(passaro.WidthRequest/2);
-		var posVpassaro = (alturaJanela/2)-(passaro.HeightRequest/2)+passaro.TranslationY;
-		var yMaxCano = canodecima.HeightRequest + canodecima.TranslationY + aberturaMinima;
-		if (posHpassaro>= Math.Abs(canodebaixo.TranslationX)-canodebaixo.WidthRequest &&
-		    posHpassaro<= Math.Abs(canodebaixo.TranslationX)+canodebaixo.WidthRequest && 
-			posVpassaro>=yMaxCano)
-			{
-				return true;
-			}
-			else
-			{
-				return false;
-			}
+		passaro.TranslationY += gravidade;
 	}
 	void AplicarPulo()
 	{
@@ -165,10 +102,81 @@ public partial class MainPage : ContentPage
 			tempoPulando = 0;
 		}
 	}
-	void OnGridClickd(object s,TappedEventArgs a)
+	bool VerificaColisao()
+	{
+		{
+			return VerificaColisaoTeto() ||
+				VerificaColisaoChao() ||
+				VerificaColisaoCanoCima()||
+				VerificaColisaoCanoBaixo();	
+		}
+	}
+	bool VerificaColisaoCano()
+	{
+		if (VerificaColisaoCanoBaixo()|| VerificaColisaoCanoCima())
+			return true;
+		else
+			return false;
+	}
+	bool VerificaColisaoCanoCima()
+	{
+		var posHpassaro = (larguraJanela/2)-(passaro.WidthRequest/2);
+		var posVpassaro = (alturaJanela/2)-(passaro.HeightRequest/2)+passaro.TranslationY;
+
+		if (
+			posHpassaro>= Math.Abs(canodecima.TranslationX)-canodecima.WidthRequest &&
+		    posHpassaro<= Math.Abs(canodecima.TranslationX)+canodecima.WidthRequest &&
+			posVpassaro<= canodecima.HeightRequest +canodecima.TranslationY
+		)
+				return true;
+			else
+				return false;
+	}
+	bool VerificaColisaoCanoBaixo()
+	{
+		var posHpassaro = (larguraJanela/2)-(passaro.WidthRequest/2);
+		var posVpassaro = (alturaJanela/2)-(passaro.HeightRequest/2)+passaro.TranslationY;
+
+		var yMaxCano = canodecima.HeightRequest + canodecima.TranslationY + aberturaMinima;
+
+		if (
+			posHpassaro>= Math.Abs(canodebaixo.TranslationX)-canodebaixo.WidthRequest &&
+		    posHpassaro<= Math.Abs(canodebaixo.TranslationX)+canodebaixo.WidthRequest && 
+			posVpassaro>=yMaxCano
+			)
+				return true;
+			else
+				return false;
+	}
+	bool VerificaColisaoChao()
+	{
+		var mixY = alturaJanela / 2; 
+		if (passaro.TranslationY >=mixY)
+	 		return true;
+		else
+			return false;
+	}
+	 private bool VerificaColisaoTeto()
+	{
+		var minY =-alturaJanela / 2;
+		if (passaro.TranslationY <=-minY)
+
+	 	    return true;
+	    else
+			return false;
+	}
+
+	private void OnGameOverClicked(object s, TappedEventArgs e)
+	{
+		frameGameOver.IsVisible = false;
+		Inicializar();
+		morto = false;
+		Desenhar();
+	}
+	
+	private void OnGridClickd(object s,TappedEventArgs e)
 	{
 		estaPulando = true;
 	}
-
 
 }
